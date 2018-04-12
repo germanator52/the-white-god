e104cd8ea8570aaa9f5588e6aacad981aac6c152
Matthew German <matthew.german@mymail.champlain.edu>
2018-04-11 21:52:45 -0400

    New scenes and art assets

---
+++ the-white-god-project/The White God/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public void StartGame()
+    {
+        if (MusicScript.instance.music.isPlaying == true)
+        {
+            MusicScript.instance.music.Stop();
+        }
+        SceneManager.LoadScene("Awaken");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void Return()
+    {
+        if (MusicScript.instance.music.isPlaying == false)
+        {
+            MusicScript.instance.music.Play();
+        }
+        SceneManager.LoadScene("Start");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ the-white-god-project/The White God/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicScript : MonoBehaviour {
+
+    public static MusicScript instance;
+
+    public AudioSource music;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        instance = this;
+        music.Play();
+        SceneManager.LoadScene("Start");
+    }
+
+    void Update()
+    {
+
+    }
+}
